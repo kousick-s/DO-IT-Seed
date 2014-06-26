042b75cd0a31d2ec5c20e0d730a56c0f949d8516
kousick <kousick.s@hashedin.com>
2014-06-26 21:37:42 +0530

    COmpleted Project Details Page

--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -2,10 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-     $scope.message = "Hello This message is from View 1";
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-    $scope.message = "Hello This message is from View 2";
+var projectcontroller = angular.module('myApp.controllers', []);
+
+projectcontroller.controller('ProjectListCtrl', ['$scope',
+  function($scope) {
+	$scope.projects = [
+	                    	    {
+	                    	        "name": "Success", 
+	                    	        "id": "GOod",
+	                    	        "collaborator": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+	                    	        
+	                    	        "description": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+	                    	    }, 
+	                    	{
+	                    	        "name": "Success1", 
+	                    	        "id": "GOod1",
+	                    	        "collaborator": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+	                    	        
+	                    	        "description": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+	                    	    }
+	                    	    
+	                    	];
+    $scope.orderProp = 'age';
   }]);
+projectcontroller.controller('ProjectDetailCtrl', ['$scope',
+                                                 function($scope) {
+                                               	
+                                                 }]);
--- DOIT/WebContent/app/js/services.js
+++ DOIT/WebContent/app/js/services.js
@@ -5,5 +5,27 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var projectServices = angular.module('myApp.services',['ngResource']);
+
+projectServices.factory('Project',
+  function(){
+	var projects=[
+	    {
+	        "name": "Success", 
+	        "id": "GOod",
+	        "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+	        
+	        "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+	    }, 
+	{
+	        "name": "Success1", 
+	        "id": "GOod1",
+	        "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+	        
+	        "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+	    }
+	    
+	];
+	
+	return projects;
+});
